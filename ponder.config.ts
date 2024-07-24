4dd41f41f359568170654d2f43b9ef996527151b
Le-Caignec <robinlecaignec@yahoo.fr>
2024-07-24 20:12:19 +0200

    fix network issue

--- ponder.config.ts
+++ ponder.config.ts
@@ -4,14 +4,14 @@ import { AppRegistryAbi } from "./abis/AppRegistry";
 
 export default createConfig({
   networks: {
-    mainnet: {
-      chainId: 1,
+    bellecour: {
+      chainId: 134,
       transport: http(process.env.PONDER_RPC_URL_1),
     },
   },
   contracts: {
     AppRegistry: {
-      network: "mainnet",
+      network: "bellecour",
       abi: AppRegistryAbi, // Use the imported ABI here
       address: "0x799DAa22654128d0C64d5b79eac9283008158730",
       startBlock: 25455501,
